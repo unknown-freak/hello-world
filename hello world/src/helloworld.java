a008e94d21d36b03ef8a44d916d4db161e71d85f
admin <admin@Bcalab3-m07>
2022-03-09 11:16:29 +0530

    commit1

---
+++ hello world/src/helloworld.java
@@ -0,0 +1,11 @@
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to\n");
+		System.out.println("my\n");
+		System.out.println("hello world");
+	}
+
+}
